c7db5628013d2eb063a8b3cff38e5e300bf4d64b
Olim2508 <rahmatovolim3@gmail.com>
2023-08-31 21:01:36 +0500

    added more jinja templates, styles

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
-const express = require("express");
+const express = require('express');
 
-const app = express()
+// express app
+const app = express();
 
-app.listen(3003)
+// listen for requests
+app.listen(3000);
 
-app.set("view engine", "ejs")
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
 
-app.get("/", (req, res) => {
-    res.render("index")
-})
+app.get('/', (req, res) => {
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
+});
 
-app.get("/about", (req, res) => {
-    res.render("about")
-})
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
 
-//redirect
-app.get("/about-me", (req, res) => {
-    res.redirect("/about")
-})
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
 
-//404
+// 404 page
 app.use((req, res) => {
-    res.render("404")
-})
+  res.status(404).render('404', { title: '404' });
+});
